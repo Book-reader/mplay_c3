7721d65fb46d241cafb6f912fee521405a1fe670
Book-reader <thevoid@outlook.co.nz>
2025-08-01 10:06:29 +1200

    initial bindings

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+		c3c.url = "github:c3lang/c3c";
+	};
+	outputs = inputs:
+	let
+		pkgs = import inputs.nixpkgs {};
+	in {
+		devShells.${builtins.currentSystem}.default = pkgs.mkShellNoCC {
+			packages = with pkgs; [ gcc gnumake valgrind gdb inputs.c3c.packages.${builtins.currentSystem}.default ];
+			buildInputs = with pkgs; [ mpv-unwrapped ];
+		};
+	};
+}
